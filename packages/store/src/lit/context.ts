54f3070c2ea306d0e0009d5aaefc529e189be3d0
Rahim <rahim.alwer@gmail.com>
2026-01-04 01:31:35 +1100

    wip

--- packages/store/src/lit/context.ts
+++
@@ -1,7 +0,0 @@
-import type { Context } from '@lit/context';
-import type { AnyStore } from '../core/store';
-
-/**
- * A Lit context typed for a store.
- */
-export type StoreContext<S extends AnyStore = AnyStore> = Context<unknown, S>;
