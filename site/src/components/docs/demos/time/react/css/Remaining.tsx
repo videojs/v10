63efe3d37b2216024e1ff85904f92c3ae6292794
Darius Cepulis <dcepulis@mux.com>
2026-02-06 15:54:19 -0600

    wip: simple demos
    closes #221
    
    eventually we'll want the compiler #245 translating one react+tailwind demo into all the relevant languages. When that time comes, our demo tooling will become more complex. Until then, each variant will be created by hand and inserted into mdx like so.
    
    ## wip
    - [ ] html demos don't work
    - [ ] style pass
    - [ ] mute button demo(s)
    - [ ] fullscreen button demo(s)
    - [ ] open new issue for post-compiler demos

---
+++ site/src/components/docs/demos/time/react/css/Remaining.tsx
@@ -0,0 +1,21 @@
+import { createPlayer, features, Time, Video } from '@videojs/react';
+
+import './Remaining.css';
+
+const Player = createPlayer({ features: [...features.video] });
+
+export default function Remaining() {
+  return (
+    <Player.Provider>
+      <Player.Container className="time-remaining">
+        <Video
+          src="https://stream.mux.com/lhnU49l1VGi3zrTAZhDm9LUUxSjpaPW9BL4jY25Kwo4/highest.mp4"
+          autoPlay
+          muted
+          loop
+        />
+        <Time.Value type="remaining" className="time-remaining__value" />
+      </Player.Container>
+    </Player.Provider>
+  );
+}
