5d162fe1f9f918f29a017d52c2bc8e52c9de0e07
Darius Cepulis <dcepulis@mux.com>
2026-02-09 15:29:26 -0600

    wip: use astro typography components

--- site/src/components/installation/SkinPickerSectionClient.tsx
+++ site/src/components/installation/SkinPickerSectionClient.tsx
@@ -2,7 +2,7 @@ import { useStore } from '@nanostores/react';
 import { useCase } from '@/stores/installation';
 import SkinPicker from './SkinPicker';
 
-export default function SkinPickerSection() {
+export default function SkinPickerSection({ children }: { children: React.ReactNode }) {
   const $useCase = useStore(useCase);
 
   // Hide for background-video use case
@@ -12,10 +12,7 @@ export default function SkinPickerSection() {
 
   return (
     <>
-      <h2 className="text-lg font-medium @lg:text-h5 @3xl:text-h4 mb-6 max-w-3xl mx-auto mt-18 [&+h3]:mt-6">
-        Choose skin
-      </h2>
-      <p className="my-6 max-w-3xl mx-auto">Choose how your player looks.</p>
+      {children}
       <SkinPicker />
     </>
   );
