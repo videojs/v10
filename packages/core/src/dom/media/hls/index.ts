811af81dfaa3b727ae13758c39bd60d67b7732ab
Wesley Luyten <me@wesleyluyten.com>
2026-03-04 12:28:15 -0800

    feat: support subtitles on source change

--- packages/core/src/dom/media/hls/index.ts
+++ packages/core/src/dom/media/hls/index.ts
@@ -3,9 +3,22 @@ import Hls from 'hls.js';
 import { type MediaDelegate, MediaDelegateMixin } from '../../../core/media/delegate';
 import { MediaProxyMixin } from '../../../core/media/proxy';
 import { CustomMediaMixin } from '../custom-media-element';
+import { HlsMediaTextTracksMixin } from './text-tracks';
 
-export class HlsMediaDelegate implements MediaDelegate {
-  #engine = new Hls();
+const defaultConfig = {
+  backBufferLength: 30,
+  renderTextTracksNatively: false,
+  liveDurationInfinity: true,
+  capLevelToPlayerSize: true,
+  capLevelOnFPSDrop: true,
+};
+
+export class HlsMediaDelegateBase implements MediaDelegate {
+  #engine = new Hls(defaultConfig);
+
+  get engine(): Hls {
+    return this.#engine;
+  }
 
   attach(target: EventTarget): void {
     this.#engine.attachMedia(target as HTMLMediaElement);
@@ -23,6 +36,8 @@ export class HlsMediaDelegate implements MediaDelegate {
     return this.#engine.url ?? '';
   }
 }
+
+const HlsMediaDelegate = HlsMediaTextTracksMixin(HlsMediaDelegateBase);
 
 // This is used by the web component because it needs to extend HTMLElement!
 export class HlsCustomMedia extends MediaDelegateMixin(
