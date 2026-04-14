96815154466d30038157c829b0720ac38fcd12b2
Wesley Luyten <me@wesleyluyten.com>
2026-04-14 10:50:46 -0500

    refactor(packages): move media host observed attributes to subclasses (#1326)

--- packages/html/src/media/mux-audio/index.ts
+++ packages/html/src/media/mux-audio/index.ts
@@ -2,4 +2,9 @@ import { CustomMediaElement } from '@videojs/core/dom/media/custom-media-element
 import { MuxAudioMedia } from '@videojs/core/dom/media/mux';
 import { MediaAttachMixin } from '../../store/media-attach-mixin';
 
-export class MuxAudio extends MediaAttachMixin(CustomMediaElement('audio', MuxAudioMedia)) {}
+export class MuxAudio extends MediaAttachMixin(CustomMediaElement('audio', MuxAudioMedia)) {
+  static get observedAttributes() {
+    // biome-ignore lint/complexity/noThisInStatic: intentional use of super
+    return [...super.observedAttributes, 'type', 'prefer-playback', 'debug'];
+  }
+}
