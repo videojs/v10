87dfe1305157b5304dfc30331dcfe8899e9845fb
Sam Potts <sam.potts@mux.com>
2026-06-22 09:46:35 +1000

    fix(html): split i18n provider from controller instance

---
+++ packages/html/src/i18n/provider-instance.ts
@@ -0,0 +1,6 @@
+import { createI18nWithBase } from './create-i18n';
+import { i18nBase } from './instance';
+
+const built = createI18nWithBase(i18nBase);
+
+export const I18nProviderMixin = built.ProviderMixin;
