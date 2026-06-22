77d7e46d687bd6ba4aa85cae8b3300bdb000ee2c
Sam Potts <sam.potts@mux.com>
2026-06-22 10:00:47 +1000

    fix(core): add light i18n base export

---
+++ packages/core/src/core/i18n/base.ts
@@ -0,0 +1,11 @@
+export {
+  canonicalLocaleRegistryKey,
+  getI18nTranslations,
+  hasRegisteredI18n,
+  localeLookupChain,
+  onI18nRegistryChange,
+  registerI18n,
+} from './registry';
+export { resolveTranslationPhrase } from './resolve-translation-phrase';
+export { createTranslator } from './translator';
+export type * from './types';
