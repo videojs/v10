3396a061b2428dcaa1b61247707fbfceada20982
Rahim <rahim.alwer@gmail.com>
2026-07-30 16:57:49 -0700

    docs(site): update button availability examples

--- site/src/components/docs/demos/airplay-button/react/css/BasicUsage.tsx
+++ site/src/components/docs/demos/airplay-button/react/css/BasicUsage.tsx
@@ -10,17 +10,9 @@ export default function BasicUsage() {
         <Video src="{{VJS10_DEMO_VIDEO_MP4}}" autoPlay muted playsInline loop />
         <AirPlayButton
           className="media-airplay-button"
-          render={(props, state) => {
-            const label =
-              state.availability === 'unsupported'
-                ? 'AirPlay not supported'
-                : state.state === 'connected'
-                  ? 'Stop AirPlay'
-                  : state.availability === 'unavailable'
-                    ? 'No AirPlay devices found'
-                    : 'Start AirPlay';
-            return <button {...props}>{label}</button>;
-          }}
+          render={(props, state) => (
+            <button {...props}>{state.state === 'connected' ? 'Stop AirPlay' : 'Start AirPlay'}</button>
+          )}
         />
       </Player.Container>
     </Player.Provider>
