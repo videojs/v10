09b9d0951773a16477160c9bb69bb7522ec704e9
Sam Potts <sam.potts@mux.com>
2026-08-18 16:30:47 +1000

    docs(site): update menu and slider references

--- site/src/docs.config.ts
+++ site/src/docs.config.ts
@@ -199,6 +199,7 @@ export const sidebar: Sidebar = [
           { slug: 'reference/use-player-context', frameworks: ['react'] },
           { slug: 'reference/use-quality-options', frameworks: ['react'] },
           { slug: 'reference/use-selector', frameworks: ['react'] },
+          { slug: 'reference/use-slider', frameworks: ['react'] },
           { slug: 'reference/use-snapshot', frameworks: ['react'] },
           { slug: 'reference/container-mixin', frameworks: ['html'] },
           { slug: 'reference/media-attach-mixin', frameworks: ['html'] },
