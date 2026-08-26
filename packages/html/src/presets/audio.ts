4bba3a62af07ee6c191dda27c90c887fb0df2a29
Rahim <rahim.alwer@gmail.com>
2026-08-25 21:06:29 -0700

    fix(html): remove preset tailwind exports

--- packages/html/src/presets/audio.ts
+++ packages/html/src/presets/audio.ts
@@ -2,6 +2,4 @@
 export { audioFeatures } from '@videojs/core/dom';
 export { AudioPlayerElement, PlayerController } from '../preset/audio/player-element';
 export { AudioSkinElement } from '../preset/audio/skin-element';
-export { AudioSkinTailwindElement } from '../preset/audio/skin-tailwind-element';
 export { MinimalAudioSkinElement } from '../preset/audio/minimal-skin-element';
-export { MinimalAudioSkinTailwindElement } from '../preset/audio/minimal-skin-tailwind-element';
