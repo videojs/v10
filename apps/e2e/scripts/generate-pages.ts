261f09ecf069ada845ec243831114cfc1879c209
Rahim <rahim.alwer@gmail.com>
2026-08-25 22:34:27 -0700

    fix(html): remove preset ui subpaths

--- apps/e2e/scripts/generate-pages.ts
+++ apps/e2e/scripts/generate-pages.ts
@@ -427,7 +427,7 @@ video.innerHTML = \`<track kind="metadata" label="thumbnails" src="\${MEDIA.${re
 poster.src = MEDIA.${resource}.poster;
 poster.alt = 'Video poster';
 
-await import('@videojs/html/video/ui');
+await import('@videojs/html/video/skin');
 `;
 }
 
