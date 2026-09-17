47a868d610d4bc5288f42a4e6598440ba81c4be6
Rahim <rahim.alwer@gmail.com>
2026-09-17 00:37:07 -0700

    fix(site): refine installation guide structure

--- site/src/components/installation/HTMLCdnCodeBlock.tsx
+++ site/src/components/installation/HTMLCdnCodeBlock.tsx
@@ -1,4 +1,5 @@
 import ClientCode from '@/components/Code/ClientCode';
+import { Tab, TabsList, TabsPanel, TabsRoot } from '@/components/Tabs';
 import { generateCdnCode } from '@/utils/installation/cdn-code';
 
 import { useSelection } from './useSelection';
@@ -13,5 +14,16 @@ export default function HTMLCdnCodeBlock({ cdnMedia }: HTMLCdnCodeBlockProps) {
   const $skin = useSelection('skin');
   const $renderer = useSelection('renderer');
 
-  return <ClientCode code={generateCdnCode($useCase, $skin, $renderer, cdnMedia)} lang="html" />;
+  return (
+    <TabsRoot maxWidth={false}>
+      <TabsList label="CDN scripts">
+        <Tab value="html" initial>
+          index.html
+        </Tab>
+      </TabsList>
+      <TabsPanel value="html" initial>
+        <ClientCode code={generateCdnCode($useCase, $skin, $renderer, cdnMedia)} lang="html" />
+      </TabsPanel>
+    </TabsRoot>
+  );
 }
